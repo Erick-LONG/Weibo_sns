01332f4ee5c04dbd114ff3924d5f72a945db032f
ErickLONG <834424581@qq.com>
2017-07-27 08:45:49 +0800

    Initial commit

---
+++ app01/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+
+# Register your models here.
+from app01 import models
+admin.site.register(models.UserProfile)
+admin.site.register(models.Comment)
+admin.site.register(models.Category)
+admin.site.register(models.Tags)
+admin.site.register(models.Topic)
+admin.site.register(models.Weibo)
